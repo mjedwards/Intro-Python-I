531b56deab4b4f9134a083ee5d4f4f8d0f995245
mjedwards <jairedwards20@gmail.com>
2020-08-07 20:08:30 -0400

    completed first assignment

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -15,21 +15,20 @@ automatically.
 
 # Example:
 
-import math
+# import math
 
-def dist(a, b):
-    """Compute the distance between two x,y points."""
-    x0, y0 = a  # Destructuring assignment
-    x1, y1 = b
+# def dist(a, b):
+#     """Compute the distance between two x,y points."""
+#     x0, y0 = a  # Destructuring assignment
+#     x1, y1 = b
 
-    return math.sqrt((x1 - x0)**2 + (y1 - y0)**2)
+#     return math.sqrt((x1 - x0)**2 + (y1 - y0)**2)
 
-a = (2, 7)   # <-- x,y coordinates stored in tuples
-b = (-14, 72)
+# a = (2, 7)   # <-- x,y coordinates stored in tuples
+# b = (-14, 72)
 
-# Prints "Distance is 66.94"
-print("Distance is: {:.2f}".format(dist(a, b)))
-
+# # Prints "Distance is 66.94"
+# print("Distance is: {:.2f}".format(dist(a, b)))
 
 
 # Write a function `print_tuple` that prints all the values in a tuple
@@ -37,8 +36,14 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 # YOUR CODE HERE
 
 t = (1, 2, 5, 7, 99)
-print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
+
+def print_tuple():
+    for a in t:
+        print(a)  # Prints 1 2 5 7 99, one per line
+
+
+print_tuple()
 # Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
-print_tuple(u)
+u = (1,)  # What needs to be added to make this work?
+print(u)
